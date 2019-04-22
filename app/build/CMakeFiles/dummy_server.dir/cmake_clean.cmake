3f054a6ef7fda2b8fcc51668e668b2ab83630742
Faza Fahleraz <ffahleraz@gmail.com>
2019-04-22 16:14:05 +0700

    Initial commit

---
+++ app/build/CMakeFiles/dummy_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dummy_server.dir/src/dummy_server.cpp.o"
+  "dummy_server.pdb"
+  "dummy_server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dummy_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ app/build/CMakeFiles/wedit.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wedit.dir/src/main.cpp.o"
+  "wedit.pdb"
+  "wedit"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wedit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
